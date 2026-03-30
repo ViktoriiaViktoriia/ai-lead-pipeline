cccc685a2c07d368fea98dc34c7457987787a826
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2026-03-30 22:18:39 +0300

    Update src/schemas/__init__.py

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,1 +1,2 @@
-from .leads_schema import BASE_COLUMNS, ENRICHED_COLUMNS
+from src.schemas.leads_schema import BASE_COLUMNS, ENRICHED_COLUMNS
+from src.schemas.prepare_schema import standardize_column_names, select_base_columns, prepare_schema
