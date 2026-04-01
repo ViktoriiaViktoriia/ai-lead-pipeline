5d327e0e4d316a796ccd1f93865e705e47d04477
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2026-04-01 23:15:38 +0300

    Update config/__init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,2 +1,3 @@
-from .logger_config import logger
-from .config import (RAW_DATA_PATH)
+from config.logger_config import logger
+from config.config import (RAW_DATA_PATH, CHUNKED_DATA_PATH, CLEANED_DATA_PATH)
+from config.variables import (LEGAL_SUFFIXES, COUNTRY_MAPPING, SIZE_MAPPING, INDUSTRY_MAPPING)
