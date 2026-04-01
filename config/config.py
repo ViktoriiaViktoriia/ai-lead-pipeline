0ce69be677609bfb22349117cc465eca97c30d21
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2026-04-01 23:17:06 +0300

    Update config/config.py: output path

--- config/config.py
+++ config/config.py
@@ -6,4 +6,6 @@ DATA_DIR = BASE_DIR / "data"
 
 RAW_DATA_PATH = DATA_DIR / "companies_raw_2023_q4.csv"
 
-CHUNKED_DATA_PATH = DATA_DIR / "processed" / "chunk_company_leads_2023_q4.csv"
+CHUNKED_DATA_PATH = DATA_DIR / "processed" / "chunked"
+
+CLEANED_DATA_PATH = DATA_DIR / "processed" / "cleaned"
