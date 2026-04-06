65c337b8aa43de6b78c947505a5dec64041cfd8a
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2026-04-06 19:53:44 +0300

    refactor(clients): expose client factory functions via package init

--- src/enrichment/__init__.py
+++ src/enrichment/__init__.py
@@ -1,5 +1,6 @@
 from src.enrichment.selection.lead_prioritizer import (assign_geo_priority, compute_priority_score,
                                                        select_top_leads)
+from src.enrichment.api_enrichment.clients import create_abstract_client, create_tech_client
 from src.enrichment.run_mode import handle_run_mode, should_process_row, normalize_data, call_api_with_retry
 from src.enrichment.process_api import process_api_batch
 from src.enrichment.enrich_company import enrich_company_chunk, enrich_company_parquet
